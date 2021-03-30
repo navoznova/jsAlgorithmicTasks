231e646ade1296b9d1f697d2e4b51632331586a3
Anna Navoznova <anna.navoznova@gmail.com>
2021-03-30 12:00:45 +0300

    Object.keys/values/entries

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-function clearArrayOfAnagrams (arr) {
-    let map = new Map ();
+function sumSalaries(salaries) {
+    let salariesArray = Object.values(salaries);
+    let sum = salariesArray.reduce((salarySum, currentsalary) =>
+        salarySum += currentsalary, 0);
+
+    return sum;
+}
+
+function clearArrayOfAnagrams(arr) {
+    let map = new Map();
 
     arr.forEach(element => {
         let key = element.toLowerCase().split('').sort().join('');
-         map.set(key, element);
+        map.set(key, element);
     });
     return Array.from(map.values());
 }
 
 function uniqueElemInArray(arr) {
-    return Array.from(new Set (arr));
+    return Array.from(new Set(arr));
 
     /*let uniqArray = [];
     let set = new Set();
@@ -19,4 +27,8 @@ function uniqueElemInArray(arr) {
         uniqArray.push(value);
     }
     return uniqArray;*/
+}
+
+function count(obj) {
+    return Object.keys(obj).length;
 }
