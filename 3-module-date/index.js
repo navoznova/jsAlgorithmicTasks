07b59517f362b3da0a09db4084f57584eeec1299
Anna Navoznova <anna.navoznova@gmail.com>
2021-04-05 14:35:12 +0300

    методы объекта Date

--- 3-module-date/index.js
+++ 3-module-date/index.js
@@ -19,6 +19,30 @@ function getLocalDay(date) {
 }
 
 function getDateAgo(date, days) {
-    let dayAgo;
+    let dateInMiliSek = date.getTime();
+    let daysInMiliSek = days * 24 * 3600 * 1000;
+
+    let dayAgo = new Date();
+    dayAgo.setTime(dateInMiliSek - daysInMiliSek);
     return dayAgo;
+}
+
+function getLastDayOfMonth(year, month) {
+    let previosDay = new Date(year, month+1, -1);
+    //let previosDay = new Date(year, month+1, 0);
+    let date = previosDay.getDate();
+    return date;
+}
+
+function getSecondsToday() {
+    let now = new Date ();
+    return now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+    
+}
+
+function getSecondsToTomorrow() {
+    let now = new Date ();
+    let tomorrow = new Date (now.getFullYear(), now.getMonth(), now.getDate()+1);
+
+    return Math.round((tomorrow - now)/1000);
 }
